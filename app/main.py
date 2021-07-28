96e082b0b5f28e6a36ff37ac287ac753d0a5effa
ayshmnmm <ayushmanmuduli19@gmail.com>
2021-07-28 21:39:21 +0530

    fixed 403 hopefully

--- app/main.py
+++ app/main.py
@@ -67,7 +67,8 @@ def api_callback():
 def cleaned_lyrics(search_query):
     print(search_query)
     URL = "https://search.azlyrics.com/search.php?q=" + search_query.replace(" ","+")
-    page = requests.get(URL)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    page = requests.get(URL,headers=headers)
     print(page)
   
     soup = BeautifulSoup(page.content, "html.parser")
